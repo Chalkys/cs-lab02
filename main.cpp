2af08b6e35dd4bf57ce70d98e053fcd9f30d5a7e
Alice (InvanovII) <alice@example.com>
2023-07-05 02:16:31 +0300

    Update: Максимальноe значение

--- main.cpp
+++ main.cpp
@@ -11,5 +11,11 @@ int main()
     cout << "A - B = " << a - b << '\n';
     cout << "A * B = " << a * b << '\n';
     cout << "A / B = " << a / b << '\n';
+    if (a > b) {
+        cout << "Max is A: " << a << '\n';
+    }
+    else {
+        cout << "Max is B: " << b << '\n';
+    }
     return 0;
 }
